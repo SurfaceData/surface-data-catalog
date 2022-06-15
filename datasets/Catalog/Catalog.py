895b6d929c3eb44be0a8f7163599e040cb518420
Keith Stevens <fozziethebeat@gmail.com>
2022-06-15 10:38:55 +0900

    moving the config

--- datasets/Catalog/Catalog.py
+++ datasets/Catalog/Catalog.py
@@ -1,19 +1,3 @@
-# coding=utf-8
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# Lint as: python3
-"""SDC: The Surface Data Collective Dataset."""
-
 import json
 config_json = """
 {
@@ -33,6 +17,22 @@ config_json = """
 }
 """
 config = json.loads(config_json)
+
+# coding=utf-8
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Lint as: python3
+"""The Surface Data Collective Dataset."""
 
 import csv
 
