17afdb6f40b7fddfc31f656672919e8747823e65
Keith Stevens <fozziethebeat@gmail.com>
2022-06-17 10:48:08 +0900

    Cleaning up unused pages

--- web/src/pages/DatasetSubset/DatasetSubsetsPage/DatasetSubsetsPage.tsx
+++
@@ -1,7 +0,0 @@
-import DatasetSubsetsCell from 'src/components/DatasetSubset/DatasetSubsetsCell'
-
-const DatasetSubsetsPage = () => {
-  return <DatasetSubsetsCell />
-}
-
-export default DatasetSubsetsPage
