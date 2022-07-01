55ac09ed3f54c63da49f1b85c9e02f94fbe13a15
Keith Stevens <fozziethebeat@gmail.com>
2022-07-01 13:16:39 +0900

    Updating translation_igboapi

--- scripts/generate_dataset_scripts.ts
+++ scripts/generate_dataset_scripts.ts
@@ -6,6 +6,74 @@ import toml from 'toml'
 
 import { db } from 'api/src/lib/db'
 
+const updateDataset = async (dataset, organization, apiUrl, homepage) => {
+  if (
+    dataset.task != 'translation' &&
+    dataset.task != 'automatic_speech_recognition'
+  ) {
+    console.log(`Skipping ${dataset.name}, task type not supported yet`)
+    return
+  }
+
+  const datasetPackage = `${dataset.task}_${dataset.id}`
+  const packageDir = `./datasets/${datasetPackage}`
+  if (!existsSync(packageDir)) {
+    console.log(`Creating HuggingFace dataset for ${datasetPackage}`)
+    execSync(
+      `huggingface-cli repo create -y ${datasetPackage} --type dataset --organization ${organization}`
+    )
+    execSync(
+      `git remote add -f ${datasetPackage} https://huggingface.co/datasets/${organization}/${datasetPackage}`
+    )
+    execSync(
+      `git subtree add --prefix datasets/${datasetPackage} ${datasetPackage} main`
+    )
+  }
+
+  console.log(`Updating dataset for ${datasetPackage}`)
+  const config = {
+    citation: '',
+    description: '',
+    homepage: homepage,
+    license: dataset.license,
+    version: 'cv-corpus-8.0-2022-01-19',
+    url: `${apiUrl}/download?dataset=${dataset.id}-{langpair}`,
+    subsets: dataset.subsets.map((subset) => {
+      const languages = subset.language.split('_')
+      return {
+        name: subset.language,
+        languages: languages,
+      }
+    }),
+  }
+  const data = {
+    config: JSON.stringify(config, null, ' '),
+  }
+  const result = await renderFile(
+    `./datasets/template_dataset/${dataset.task}.py`,
+    data
+  )
+  writeFileSync(`${packageDir}/${datasetPackage}.py`, result)
+
+  const gitStatus = await simpleGit().status([packageDir])
+  if (gitStatus.modified.length == 0 && gitStatus.not_added.length == 0) {
+    return
+  }
+  if (gitStatus.not_added.length >= 0) {
+    execSync(`git add ${packageDir}`)
+  }
+  try {
+    console.log(gitStatus)
+    execSync(`git commit -am "Updating ${datasetPackage}"`)
+    execSync(
+      `git subtree push --prefix datasets/${datasetPackage} ${datasetPackage} main`
+    )
+  } catch (error) {
+    console.log(error.stdout.toString('utf8'))
+    console.log(error.stderr.toString('utf8'))
+  }
+}
+
 export default async ({ args }) => {
   const tomlFile = readFileSync('redwood.toml', 'utf8')
   const options = toml.parse(tomlFile)
@@ -18,71 +86,7 @@ export default async ({ args }) => {
       subsets: true,
     },
   })
-  datasets.forEach(async (dataset) => {
-    if (
-      dataset.task != 'translation' &&
-      dataset.task != 'automatic_speech_recognition'
-    ) {
-      console.log(`Skipping ${dataset.name}, task type not supported yet`)
-      return
-    }
-
-    const datasetPackage = `${dataset.task}_${dataset.id}`
-    const packageDir = `./datasets/${datasetPackage}`
-    if (!existsSync(packageDir)) {
-      console.log(`Creating HuggingFace dataset for ${datasetPackage}`)
-      execSync(
-        `huggingface-cli repo create -y ${datasetPackage} --type dataset --organization ${organization}`
-      )
-      execSync(
-        `git remote add -f ${datasetPackage} https://huggingface.co/datasets/${organization}/${datasetPackage}`
-      )
-      execSync(
-        `git subtree add --prefix datasets/${datasetPackage} ${datasetPackage} main`
-      )
-    }
-
-    console.log(`Updating dataset for ${datasetPackage}`)
-    const config = {
-      citation: '',
-      description: '',
-      homepage: homepage,
-      license: dataset.license,
-      version: 'cv-corpus-8.0-2022-01-19',
-      url: `${apiUrl}/download?dataset=${dataset.id}-{langpair}`,
-      subsets: dataset.subsets.map((subset) => {
-        const languages = subset.language.split('_')
-        return {
-          name: subset.language,
-          languages: languages,
-        }
-      }),
-    }
-    const data = {
-      config: JSON.stringify(config, null, ' '),
-    }
-    const result = await renderFile(
-      `./datasets/template_dataset/${dataset.task}.py`,
-      data
-    )
-    writeFileSync(`${packageDir}/${datasetPackage}.py`, result)
-
-    const gitStatus = await simpleGit().status([packageDir])
-    if (gitStatus.modified.length == 0 && gitStatus.not_added.length == 0) {
-      return
-    }
-    if (gitStatus.not_added.length >= 0) {
-      execSync(`git add ${packageDir}`)
-    }
-    try {
-      console.log(gitStatus)
-      execSync(`git commit -am "Updating ${datasetPackage}"`)
-      execSync(
-        `git subtree push --prefix datasets/${datasetPackage} ${datasetPackage} main`
-      )
-    } catch (error) {
-      console.log(error.stdout.toString('utf8'))
-      console.log(error.stderr.toString('utf8'))
-    }
-  })
+  for (const dataset of datasets) {
+    await updateDataset(dataset, organization, apiUrl, homepage)
+  }
 }
